d2cc43ff9c2a3a39f541d770a54f471cf8863d5e
Anthony Blackshaw <ant@getme.co.uk>
2018-03-28 19:30:42 +0100

    Ready for release

--- module/index.js
+++ module/index.js
@@ -13,10 +13,10 @@ export class ManhattanManifestPlugin {
 
         this.options = Object.assign(
             {
-                exclude: null,
-                filepath: 'static-assets.json',
-                presets: {},
-                stripHash: (filepath) => {
+                'exclude': null,
+                'filepath': 'static-assets.json',
+                'presets': {},
+                'stripHash': (filepath) => {
                     let parts = filepath.split('.')
                     parts.splice(parts.length - 2, 1)
                     return parts.join('.')
@@ -29,21 +29,21 @@ export class ManhattanManifestPlugin {
 
     apply(compiler) {
 
-        compiler.hooks.beforeRun.tapAsync(pluginName, (compilation, done) => {
+        compiler.hooks.beforeRun.tap(pluginName, (compilation, done) => {
             // Initially remove the output path directory
-            rimraf(compiler.options.output.path, done)
+            rimraf.sync(compiler.options.output.path, done)
         })
 
         compiler.hooks.done.tap(pluginName, (compilation) => {
 
             const basePath = compiler.options.output.path
-            const publicPath = compiler.options.output.publicPath
+            const {publicPath} = compiler.options.output
 
             // Compile and write out a manifest of assets within the output
             // path.
             const manifest = Object.assign({}, this.options.presets)
             const walker = walk.walk(basePath)
-            const exclude = this.options.exclude
+            const {exclude} = this.options
 
             walker.on('file', (root, fileStats, next) => {
                 const relpath = path.relative(basePath, root)
@@ -51,14 +51,19 @@ export class ManhattanManifestPlugin {
 
                 // Don't add excluded files to the manifest
                 if (exclude && exclude.test(filepath)) {
-                    return next()
+                    next()
+                    return
+                }
+
+                // Check an unhashed version of the filepath exists
+                const filename = this.options.stripHash(filepath)
+                if (filename === null) {
+                    next()
+                    return
                 }
 
                 // Add an entry to the manifest for the file
-                const lookupPath = path.join(
-                    '/',
-                    this.options.stripHash(filepath)
-                )
+                const lookupPath = path.join('/', filename)
                 const storedPath = publicPath + filepath
                 manifest[lookupPath] = storedPath
                 next()
